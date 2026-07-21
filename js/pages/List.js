906c2215e5a765041d74efdebb699341d736a6a1
sunckle <sprunkibfdipingoofy@gmail.com>
2026-07-21 14:50:46 -0700

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,7 +100,7 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Achieved the record without using hacks but 1000 tps is allowed for the level "please no" (on other levels, FPS bypass is allowed, up to 360fps)
                     </p>
                     <p>
                         Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
@@ -121,6 +121,7 @@ export default {
                         Do not use easy modes, only a record of the unmodified level qualifies
                     </p>
                     <p>
+                        Once a level falls into legacy, we don't accept records for them.
                 </div>
             </div>
         </main>
