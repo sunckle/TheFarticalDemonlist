88e4cddccfdaec9ff032887ed2dd18b142c77b69
sunckle <sprunkibfdipingoofy@gmail.com>
2026-07-28 12:47:21 -0700

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -28,100 +28,204 @@ export default {
                             <p v-if="i + 1 <= 50" class="type-label-lg">#{{ i + 1 }}</p>
                             <p v-else class="type-label-lg">Legacy</p>
                         </td>
-                        <td class="level" :class="{ 'active': selected == i, 'error': !level }">
+                        <td class="level" :class="{ active: selected == i, error: !level }">
                             <button @click="selected = i">
-                                <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
+                                <span class="type-label-lg">
+                                    {{ level?.name || \`Error (\${err}.json)\` }}
+                                </span>
                             </button>
                         </td>
                     </tr>
                 </table>
             </div>
+
             <div class="level-container">
-                <div class="level" v-if="level">
-                    <h1>{{ level.name }}</h1>
-                    <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
-                    <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
-                    <ul class="stats">
-                        <li>
-                            <div class="type-title-sm">Points when completed</div>
-                            <p>{{ score(selected + 1, 100, level.percentToQualify) }}</p>
-                        </li>
-                        <li>
-                            <div class="type-title-sm">ID</div>
-                            <p>{{ level.id }}</p>
-                        </li>
-                        <li>
-                            <div class="type-title-sm">Password</div>
-                            <p>{{ level.password || 'Free to Copy' }}</p>
-                        </li>
-                    </ul>
-                    <h2>Records</h2>
-                    <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
-                    <p v-else-if="selected +1 <= 150"><strong>100%</strong> or better to qualify</p>
-                    <p v-else>This level does not accept new records.</p>
-                    <table class="records">
-                        <tr v-for="record in level.records" class="record">
-                            <td class="percent">
-                                <p>{{ record.percent }}%</p>
-                            </td>
-                            <td class="user">
-                                <a :href="record.link" target="_blank" class="type-label-lg">{{ record.user }}</a>
-                            </td>
-                            <td class="mobile">
-                                <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
-                            </td>
-                            <td class="hz">
-                                <p>{{ record.hz }}Hz</p>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                <div v-else class="level" style="height: 100%; justify-content: center; align-items: center;">
+
+                <transition name="page" mode="out-in">
+
+                    <div class="level" v-if="level" :key="selected">
+
+                        <h1>{{ level.name }}</h1>
+
+                        <LevelAuthors
+                            :author="level.author"
+                            :creators="level.creators"
+                            :verifier="level.verifier">
+                        </LevelAuthors>
+
+                        <iframe
+                            class="video"
+                            id="videoframe"
+                            :src="video"
+                            frameborder="0">
+                        </iframe>
+
+                        <ul class="stats">
+                            <li>
+                                <div class="type-title-sm">
+                                    Points when completed
+                                </div>
+                                <p>{{ score(selected + 1, 100, level.percentToQualify) }}</p>
+                            </li>
+
+                            <li>
+                                <div class="type-title-sm">ID</div>
+                                <p>{{ level.id }}</p>
+                            </li>
+
+                            <li>
+                                <div class="type-title-sm">Password</div>
+                                <p>{{ level.password || 'Free to Copy' }}</p>
+                            </li>
+                        </ul>
+
+                        <h2>Records</h2>
+
+                        <p v-if="selected + 1 <= 75">
+                            <strong>{{ level.percentToQualify }}%</strong>
+                            or better to qualify
+                        </p>
+
+                        <p v-else-if="selected + 1 <= 150">
+                            <strong>100%</strong>
+                            or better to qualify
+                        </p>
+
+                        <p v-else>
+                            This level does not accept new records.
+                        </p>
+
+                        <table class="records">
+
+                            <tr
+                                v-for="record in level.records"
+                                class="record">
+
+                                <td class="percent">
+                                    <p>{{ record.percent }}%</p>
+                                </td>
+
+                                <td class="user">
+                                    <a
+                                        :href="record.link"
+                                        target="_blank"
+                                        class="type-label-lg">
+
+                                        {{ record.user }}
+
+                                    </a>
+                                </td>
+
+                                <td class="mobile">
+                                    <img
+                                        v-if="record.mobile"
+                                        :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`"
+                                        alt="Mobile">
+                                </td>
+
+                                <td class="hz">
+                                    <p>{{ record.hz }}Hz</p>
+                                </td>
+
+                            </tr>
+
+                        </table>
+
+                    </div>
+
+                </transition>
+
+                <div
+                    v-else
+                    class="level"
+                    style="height:100%;justify-content:center;align-items:center;"
+                >
                     <p>(ノಠ益ಠ)ノ彡┻━┻</p>
                 </div>
             </div>
+
             <div class="meta-container">
                 <div class="meta">
                     <div class="errors" v-show="errors.length > 0">
                         <p class="error" v-for="error of errors">{{ error }}</p>
                     </div>
+
                     <div class="og">
-                        <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
+                        <p class="type-label-md">
+                            Website layout made by
+                            <a href="https://tsl.pages.dev/" target="_blank">
+                                TheShittyList
+                            </a>
+                        </p>
                     </div>
+
                     <template v-if="editors">
                         <h3>List Editors</h3>
+
                         <ol class="editors">
                             <li v-for="editor in editors">
-                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
-                                <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
+                                <img
+                                    :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`"
+                                    :alt="editor.role"
+                                />
+
+                                <a
+                                    v-if="editor.link"
+                                    class="type-label-lg link"
+                                    target="_blank"
+                                    :href="editor.link"
+                                >
+                                    {{ editor.name }}
+                                </a>
+
                                 <p v-else>{{ editor.name }}</p>
                             </li>
                         </ol>
                     </template>
+
                     <h3>Submission Requirements</h3>
+
                     <p>
-                        Achieved the record without using hacks but 1000 tps is allowed for the level "please no" (on other levels, FPS bypass is allowed, up to 360fps)
+                        Achieved the record without using hacks but 1000 tps is
+                        allowed for the level "please no" (on other levels,
+                        FPS bypass is allowed, up to 360fps)
                     </p>
+
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        Achieved the record on the level that is listed on the
+                        site - please check the level ID before you submit a
+                        record
                     </p>
+
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Have either source audio or clicks/taps in the video.
+                        Edited audio only does not count
                     </p>
+
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        The recording must have a previous attempt and entire
+                        death animation shown before the completion, unless the
+                        completion is on the first attempt. Everyplay records
+                        are exempt from this
                     </p>
+
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        The recording must also show the player hit the endwall,
+                        or the completion will be invalidated.
                     </p>
+
                     <p>
                         Do not use secret routes or bug routes
                     </p>
+
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        Do not use easy modes, only a record of the unmodified
+                        level qualifies
                     </p>
+
                     <p>
-                        Once a level falls into legacy, we don't accept records for them.
+                        Once a level falls into legacy, we don't accept records
+                        for them.
                     </p>
                 </div>
             </div>
@@ -132,6 +236,7 @@ export default {
         editors: [],
         loading: true,
         selected: 0,
+        animateLevel: true,
         errors: [],
         roleIconMap,
         store
@@ -153,11 +258,9 @@ export default {
         },
     },
     async mounted() {
-        // Hide loading spinner
         this.list = await fetchList();
         this.editors = await fetchEditors();
 
-        // Error handling
         if (!this.list) {
             this.errors = [
                 "Failed to load list. Retry in a few minutes or notify list staff.",
@@ -170,6 +273,7 @@ export default {
                         return `Failed to load level. (${err}.json)`;
                     })
             );
+
             if (!this.editors) {
                 this.errors.push("Failed to load list editors.");
             }
