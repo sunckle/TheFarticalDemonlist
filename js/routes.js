23fce4dd7d4397328308efad5e993a9d28c71b01
sunckle <sprunkibfdipingoofy@gmail.com>
2026-07-28 13:17:20 -0700

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import Changelog from './pages/Changelog.js';
 
 export default [
   { path: '/', component: List },
   { path: '/leaderboard', component: Leaderboard },
   { path: '/roulette', component: Roulette },
+  { path: '/changelog', component: Changelog },
 ];
